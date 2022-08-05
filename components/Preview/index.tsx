e2c1a659ba80cd19c7fd63604f5487743c340f05
Michael Hungbo <hungbomichael@gmail.com>
2022-08-05 11:53:38 +0100

    refactor: refactor styles

--- components/Preview/index.tsx
+++ components/Preview/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
 type DataProps = {
+   
     data: {
+    title: string,
     description: string,
     url: string
     preview: string,
@@ -11,17 +13,28 @@ type DataProps = {
 
 const Preview = ({data} : DataProps) => {
   return (
-      <>
-             <a href={data.url} className= "" >
-                <div className= "h-60 w-64 bg-[#080910] outline-none text-gray-500 rounded cursor-pointer overflow-hidden">           
-              <div>
-                  <p className="text-sm">{data.description}</p>
-              </div>
-
+    <>
+      <a href={data.url} className="">
+        <div className="h-60 w-64 bg-[#080910] outline-none text-gray-500 rounded cursor-pointer overflow-hidden">
+          <div className='image h-[70%]'>
+              
           </div>
-          </a>
-      </>
-  )
+          
+          <div className='h-[30%] pt-1 bg-gray-500 text-black flex flex-col place-items-center overflow-hidden'>
+            <div>
+              <h2 className="text-sm font-bold uppercase">{data.title}</h2>
+            </div>
+            <div>
+                <p className=" text-ellipsis text-sm">{data.description}</p>
+            </div>
+            <div className ="">
+                
+            </div>
+          </div>
+        </div>
+      </a>
+    </>
+  );
 }
 
 export default Preview
