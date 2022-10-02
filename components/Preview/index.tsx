3f72e93ccf5455b7899140becb3bfba0901697aa
Michael Hungbo <hungbomichael@gmail.com>
2022-10-02 20:56:30 +0100

    feat: add aos

--- components/Preview/index.tsx
+++ components/Preview/index.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useEffect} from 'react'
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
 
 type DataProps = {  
     data: {
@@ -11,9 +13,13 @@ type DataProps = {
 }
 
 const Preview = ({data} : DataProps) => {
+  useEffect(() => {
+     AOS.init()
+   })
+
   return (
     <>
-      <a href={data.url} className="shadow-lg shadow-gray-800/50 hover:scale-105 hover:translate-y-1 transition-all duration-300 ease-in-out">
+      <a data-aos="fade-up" href={data.url} className="shadow-lg shadow-gray-800/50 hover:scale-105 hover:translate-y-1 transition-all duration-300 ease-in-out">
         <div className="h-60 w-64 bg-[#080910] outline-none text-gray-500 rounded cursor-pointer overflow-hidden">
           <div className='image h-[70%]'>
             <img src={data.imgURL} alt={`${data.title} landing page`} className="h-full w-full"/>
@@ -21,7 +27,7 @@ const Preview = ({data} : DataProps) => {
           
           <div className='h-[30%] pt-1 bg-black text-gray-300 flex flex-col place-items-center overflow-hidden'>
             <div>
-              <h2 className="text-sm font-bold uppercase">{data.title}</h2>
+              <h2 className="text-sm font-bold capitalize">{data.title}</h2>
             </div>
             <div className="text-center">
                 <p className="text-ellipsis text-sm">{data.description}</p>
