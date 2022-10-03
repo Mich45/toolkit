9c0bc17f6211e0c269188a42f187f16ddc93640f
Michael Hungbo <hungbomichael@gmail.com>
2022-10-03 16:39:42 +0100

    refactor: refactor styles

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
+    loader: 'imgix',
+    path: 'https://res.cloudinary.com/cloudinarymich/',
     domains: ['res.cloudinary.com']
   }
 }
