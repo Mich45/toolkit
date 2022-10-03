9104e7400346f835d56180daff8fa121101a6f3d
Michael Hungbo <hungbomichael@gmail.com>
2022-10-03 15:16:10 +0100

    refactor: refactor styles

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <>
       <div className="h-24 w-full bg-[#080910]">
-        <div className="lg:mx-[130px] text-sm flex-col flex lg:flex-row lg:justify-between items-center text-gray-300">
+        <div className="lg:mx-[130px] h-full text-sm flex-col flex lg:flex-row lg:justify-between items-center text-gray-300">
           <div className="py-2">
             <p>With 💜 by <a className="text-slate-700 hover:text-gray-500 transition ease-in-out"  href="https://twitter.com/michaelhungbo">Michael Hungbo</a></p>
           </div>
