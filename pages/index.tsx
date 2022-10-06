0b1068a81f12436dad6b05179177cd91f9eb9b63
Michael Hungbo <hungbomichael@gmail.com>
2022-10-06 10:28:16 +0100

    refactor: refactor styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,7 @@ const Home = ({tools}: any) => {
           <div className="flex items-center place-content-center p-5">
             <div className="">
               <h1 className="text-6xl mt-10 py-2 text-gray-400 font-bold font-[cursive] ">
-                ToolKit <span style={{ display: "inlineBlock", verticalAlign: "bottom" }}><img src={Toolkit.src} style={{verticalAlign: "bottom"}} height={55} width={55} /></span>
+                ToolKit <span style={{ display: "inline-block", verticalAlign: "bottom" }}><img src={Toolkit.src} style={{verticalAlign: "bottom"}} height={55} width={55} /></span>
               </h1>
             </div>
           </div>
@@ -49,7 +49,7 @@ const Home = ({tools}: any) => {
             <div className="tag">✒ Icons</div>
             <div className="tag">📑 Cheatsheets</div>
           </div>
-          <div className=" w-4/5 py-10 my-0 mx-auto toolsWrapper flex flex-col lg:grid lg:grid-cols-4 gap-5 items-center place-content-center">
+          <div className=" w-4/5 py-10 my-0 mx-auto toolsWrapper flex flex-col lg:grid lg:grid-cols-4 md:grid md:grid-cols-2 gap-5 items-center place-content-center">
             {  
               tools.map((data: any, key: any) => {
                 return <Preview key={key} data={data} />
